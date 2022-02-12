b0c459c0a95ed1f1815c14778625af10698f3b49
vieiramauricio <mauriciorenanvs@gmail.com>
2022-02-12 17:28:10 -0300

    :building_construction: added typescript and cleaned the structure
    added typescript and cleaned the structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>dashgo</title>
+        <meta name="description" content="dashgo" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>dashgo</h1>
+    </>
+  )
+}
