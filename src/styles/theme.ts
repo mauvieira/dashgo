e68c02352c305c4018e66d8034469565a42b13a6
vieiramauricio <mauriciorenanvs@gmail.com>
2022-02-12 17:56:52 -0300

    :lipstick: added Roboto font
    added Roboto font

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -15,11 +15,15 @@ export const theme = extendTheme({
       "50": "#EEEEF2",
     }
   },
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto',
+  },
   styles: {
     global: {
       body: {
         bg: 'gray.900',
-        color: 'gray.50'
+        color: 'gray.50',
       }
     }
   }
