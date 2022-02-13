5ca54e42d303b6e29970455c46f10b5d006c49a1
vieiramauricio <mauriciorenanvs@gmail.com>
2022-02-13 16:07:31 -0300

    :sparkles: added sidebar component
    added sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,15 @@
+import { Flex } from "@chakra-ui/react"
 import { Header } from "../components/Header"
+import { Sidebar } from "../components/Sidebar"
 
 export default function Dashboard() {
-  return <Header />
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" maxWidth={1480} my="6" mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  )
 }
