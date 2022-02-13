fb5007fcc73e6490309da75d9598b1f8f5c81363
vieiramauricio <mauriciorenanvs@gmail.com>
2022-02-12 21:58:09 -0300

    :sparkles: added react iconsand finished search style
    added react iconsand finished search style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Input, Text, Icon } from "@chakra-ui/react";
+import { RiSearchLine } from "react-icons/ri"
 
 export function Header() {
   return (
@@ -30,6 +31,34 @@ export function Header() {
           .
         </Text>
       </Text>
+
+      <Flex
+        as="label"
+        flex="1"
+        py="4"
+        px="8"
+        ml="6"
+        maxWidth={400}
+        alignSelf="center"
+        color="gray.200"
+        position="relative"
+        bg="gray.800"
+        borderRadius="full"
+        align="center"
+      >
+        <Input
+          color="gray.50"
+          variant="unstyled"
+          px="4"
+          mr="4"
+          placeholder="Search on the app"
+          _placeholder={{
+            color: "gray.400"
+          }}
+        />
+
+        <Icon as={RiSearchLine} />
+      </Flex>
     </Flex>
   )
 }
