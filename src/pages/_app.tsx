fd0c50aa3c12202bb70c0af8634b0aa90ca59ad3
vieiramauricio <mauriciorenanvs@gmail.com>
2022-02-22 22:04:52 -0300

    :bug: fixed namespace
    fixed namespace

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 import { SidebarDrawerProvider } from '../context/SidebarDrawerContext';
-import { makeServer } from '../services';
+import { makeServer } from '../services/mirage';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools'
 
