efab30342cbba346bffc6a0b439941f10bf5beb3
vieiramauricio <mauriciorenanvs@gmail.com>
2022-02-22 22:13:05 -0300

    :pencil2: type: moved the type users to the get function
    type: moved the type users to the get function

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -8,23 +8,26 @@ type User = {
   createdAt: string;
 }
 
+async function getUsers(): Promise<User[]> {
+  const { data } = await api.get('/users');
+
+  const users = data.users.map(({ id, name, email, createdAt }: User) => ({
+    id,
+    name,
+    email,
+    createdAt: new Date(createdAt).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  }))
+
+  return users;
+
+}
+
 export function useUsers() {
-  return useQuery('users', async () => {
-    const { data } = await api.get('/users');
-
-    const users = data.users.map(({ id, name, email, createdAt }: User) => ({
-      id,
-      name,
-      email,
-      createdAt: new Date(createdAt).toLocaleDateString('pt-BR', {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric'
-      })
-    }))
-
-    return users;
-  }, {
-    staleTime: 1000 * 5
-  });
+  return useQuery('users', getUsers, {
+    staleTime: 1000 * 5 // 5 seconds
+  })
 }
